d8cde46b39f4c0ba1f3299cd49b80d1fa22613fd
KarzLightyear <k.k.yamato@gmail.com>
2021-06-08 12:50:08 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '75cac116225e10ca1bdc9d054ea0c04c7347ab630e2f67f523465e42e2d2325a5026b980a077705208fcb5571ae92acda3ef03cbcdb366c4071faf3d88305972'
